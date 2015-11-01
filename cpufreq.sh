88dbdc59fb844160441b2690338b7d15e3ad1732
Kristofer Karlsson <krka@spotify.com>
2015-11-01 21:00:09 +0100

    Add option to look for screensaver instead of playback

--- cpufreq.sh
+++ cpufreq.sh
@@ -11,29 +11,56 @@ fi
 
 PROCESSORS=$(cat /proc/cpuinfo |grep "^processor"|cut -d : -f 2|cut -b 2-| tr '\n' ' ')
 
+CHECK=screensaver
+
 while true ; do
     lockfile-touch --oneshot /var/run/cpufreq.pid
 
-	result=$(curl -s -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}' http://localhost:8080/jsonrpc)
+    if [ $CHECK == "playing" ]; then
+      # Check if a video is playing
+	  result=$(curl -s -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}' http://localhost:8080/jsonrpc)
+    elif [ $CHECK == "screensaver" ]; then
+      # Check if screensaver is active
+	  result=$(curl -s -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "XBMC.GetInfoBooleans", "params": { "booleans": ["System.ScreenSaverActive "] }, "id": 1}' http://localhost:8080/jsonrpc)
+    else
+	  echo "$(date): invalid check option: $CHECK"
+	  exit 1
+    fi
+
 
 	if [ $? -ne 0 ] ; then
 	  echo "$(date): xbmc not running"
 	  exit 1
 	fi
 
-	playing=$(echo $result | jq '.result | length')
+    if [ $CHECK == "playing" ]; then
+  	  playing=$(echo $result | jq '.result | length')
+	  if [ $? -ne 0 ] ; then
+	    echo "$(date): xbmc response was not json: $result"
+	    exit 1
+	  fi
 
-	if [ $? -ne 0 ] ; then
-	  echo "$(date): xbmc response was not json: $result"
-	  exit 1
-	fi
+	  if [ $playing -ge 1 ] ; then
+	    wanted=performance
+	  else
+	    wanted=powersave
+	  fi
+    elif [ $CHECK == "screensaver" ]; then
+  	  screensaver=$(echo $result | jq '.result["System.ScreenSaverActive "]')
+	  if [ $? -ne 0 ] ; then
+	    echo "$(date): xbmc response was not json: $result"
+	    exit 1
+	  fi
 
-	if [ $playing -ge 1 ] ; then
-	  wanted=performance
-	else
-	  wanted=powersave
-	fi
-
+	  if [ $screensaver == "true" ] ; then
+	    wanted=powersave
+	  else
+	    wanted=performance
+	  fi
+    else
+	  echo "$(date): invalid check option: $CHECK"
+    fi
+ 
     modified=0
     for p in $PROCESSORS; do
   	  current=$(cpufreq-info -c $p | grep "The governor"|cut -f 2 -d '"')
